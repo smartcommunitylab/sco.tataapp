6d3c428ddf290137b7179c46f3aca3c9f754e80b
oskarnrk <o.zambotti@fbk.eu>
2016-04-14 12:04:42 +0200

    Floating button!

--- tataapp-mobile/www/js/controllers/HomeCtrl.js
+++ tataapp-mobile/www/js/controllers/HomeCtrl.js
@@ -1,5 +1,18 @@
 angular.module('tataapp.controllers.home', [])
 
-.controller('AppCtrl', function ($scope) {})
+.controller('AppCtrl', function ($scope, $state, $ionicHistory) {
+    // alternative to <a href>
+    $scope.goTo = function (state, refresh, root) {
+        if (root) {
+            $ionicHistory.nextViewOptions({
+                historyRoot: true
+            });
+        }
+
+        $state.go(state, {}, {
+            refresh: !!refresh
+        });
+    };
+})
 
 .controller('HomeCtrl', function ($scope) {});
