ee6fff6f2bad1f65d413dbe7ef93d7a1c7d5075f
S4mu3l3P <samuelepinginf@gmail.com>
2016-05-03 11:40:41 +0200

    useless

--- tataapp-mobile/www/js/controllers/PointsCtrl.js
+++ tataapp-mobile/www/js/controllers/PointsCtrl.js
@@ -1,13 +1,12 @@
 angular.module('tataapp.controllers.points', [])
 
-.controller('PointsCtrl', function ($scope, $filter, BackendSrv, Config, $q) {
+.controller('PointsCtrl', function ($scope, $filter, BackendSrv, Config) {
     var now = new Date();
     $scope.dateFormat = Config.dateFormatNum;
     $scope.dateFormatMonth = Config.dateFormatMonth
     $scope.tataPoints = [];
     $scope.month = now;
     var getAllTatapoint = function (agencyId) {
-        var deferred = $q.defer();
         BackendSrv.getAllTatapoint(agencyId).then(
             function (data) {
                 $scope.tataPoints = data.content;
