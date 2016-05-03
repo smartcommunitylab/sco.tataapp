f7e27ba5b105f11dd9668bf0a5b302e61f6587a2
oskarnrk <o.zambotti@fbk.eu>
2016-05-03 11:01:25 +0200

    ConfigSrv to Config

--- tataapp-mobile/www/js/services/BackendSrv.js
+++ tataapp-mobile/www/js/services/BackendSrv.js
@@ -3,144 +3,144 @@ angular.module('tataapp.services.backend', [])
 .factory('BackendSrv', function ($rootScope, $http, $q, Config) {
     var backend = {};
 
-    /*Get all tate*/
+    /* Get all tate */
     backend.getAllTate = function (agencyId) {
         var deferred = $q.defer();
 
-        $http.get(ConfigSrv.getServerURL() + '/api/agency/'+agencyId+'/tata', ConfigSrv.getHTTPConfig())
+        $http.get(Config.getServerURL() + '/api/agency/' + agencyId + '/tata', Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Get a single tata*/
+    /* Get a single tata */
     backend.getATata = function (agencyId, babysitterId) {
         var deferred = $q.defer();
 
-        $http.get(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/tata/' + babysitterId, ConfigSrv.getHTTPConfig())
+        $http.get(Config.getServerURL() + '/api/agency/' + agencyId + '/tata/' + babysitterId, Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Search tate*/
+    /* Search tate */
     backend.deleteTata = function (agencyId, tate) {
         var deferred = $q.defer();
 
-        $http.post(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/tata/search', tate, ConfigSrv.getHTTPConfig())
+        $http.post(Config.getServerURL() + '/api/agency/' + agencyId + '/tata/search', tate, Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Get tariffario*/
+    /* Get tariffario */
     backend.getTariffario = function (agencyId) {
         var deferred = $q.defer();
 
-        $http.get(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/pricelist', ConfigSrv.getHTTPConfig())
+        $http.get(Config.getServerURL() + '/api/agency/' + agencyId + '/pricelist', Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Calcolo preventivo*/
+    /* Calcolo preventivo */
     backend.getPreventivo = function (agencyId, preventivo) {
         var deferred = $q.defer();
 
-        $http.post(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/estimation', preventivo, ConfigSrv.getHTTPConfig())
+        $http.post(Config.getServerURL() + '/api/agency/' + agencyId + '/estimation', preventivo, Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Richiesta colloquio*/
+    /* Richiesta colloquio */
     backend.getPreventivo = function (agencyId, colloquio) {
         var deferred = $q.defer();
 
-        $http.post(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/meeting', colloquio, ConfigSrv.getHTTPConfig())
+        $http.post(Config.getServerURL() + '/api/agency/' + agencyId + '/meeting', colloquio, Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Get all tatapoint*/
+    /* Get all tatapoint */
     backend.getAllTatapoint = function (agencyId) {
         var deferred = $q.defer();
 
-        $http.get(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/tatapoint', ConfigSrv.getHTTPConfig())
+        $http.get(Config.getServerURL() + '/api/agency/' + agencyId + '/tatapoint', Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
         return deferred.promise;
     };
 
-    /*Get a tatapoint*/
+    /* Get a tatapoint */
     backend.getATatapoint = function (agencyId, tatapointId) {
         var deferred = $q.defer();
 
-        $http.get(ConfigSrv.getServerURL() + '/api/agency/' + agencyId + '/tatapoint/', tatapointId, ConfigSrv.getHTTPConfig())
+        $http.get(Config.getServerURL() + '/api/agency/' + agencyId + '/tatapoint/', tatapointId, Config.getHTTPConfig())
 
         .then(
             function (response) {
                 deferred.resolve(response.data);
             },
-            function (responseError) {
-                deferred.reject(responseError.data ? responseError.data.errorMessage : responseError);
+            function (reason) {
+                deferred.reject(reason.data ? reason.data.errorMessage : reason);
             }
         );
 
