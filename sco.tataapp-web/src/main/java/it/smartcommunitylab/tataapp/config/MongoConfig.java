c0c54783941cef906974e66b6bffd84661e936b0
mirkoperillo <mirko.perillo@gmail.com>
2016-05-03 16:12:31 +0200

    fix to use boot starter mongodb

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/config/MongoConfig.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/config/MongoConfig.java
@@ -2,14 +2,13 @@ package it.smartcommunitylab.tataapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 import com.mongodb.MongoClient;
 
-@Configuration
+//@Configuration
 public class MongoConfig {
 
 	public static final String DEFAULT_DB_NAME = "tataapp";
