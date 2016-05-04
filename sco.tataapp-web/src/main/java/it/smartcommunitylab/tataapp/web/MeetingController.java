bac1c1affd0dbc59bc41884e2382f5d6c35419e4
mirkoperillo <mirko.perillo@gmail.com>
2016-05-04 17:23:49 +0200

    try to fix CORS

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/MeetingController.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/MeetingController.java
@@ -1,6 +1,7 @@
 package it.smartcommunitylab.tataapp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import it.smartcommunitylab.tataapp.model.Meeting;
 import it.smartcommunitylab.tataapp.service.MeetingService;
 
 @RestController
+@CrossOrigin()
 public class MeetingController {
 
 	@Autowired
