b110b66f3159bacb64cad3197f9c958860bc163b
Mattia Bortolamedi <regolo85@gmail.com>
2016-05-10 16:37:27 +0200

    First graphical update in tata pages

--- sco.tataapp-web/src/main/resources/public/controllers.js
+++ sco.tataapp-web/src/main/resources/public/controllers.js
@@ -1,6 +1,56 @@
 'use strict';
 
 angular.module('app.ctrls', [])
-.controller('HomeCtrl', ['$scope',function($scope) {
+.controller('MainCtrl', ['$scope',function($scope) {
 	$scope.message = "Angular ROCKSSS";
+	
+	var activeHome = "active";
+	var activeTata = "";
+	var activeFamily = "";
+	var activeTataPoint = "";
+	
+	$scope.isActiveHome = function(){
+		return activeHome;
+	};
+	
+	$scope.isActiveTata = function(){
+		return activeTata;
+	};
+	
+	$scope.isActiveFamily = function(){
+		return activeFamily;
+	};
+	
+	$scope.isActiveTataPoint = function(){
+		return activeTataPoint;
+	};
+	
+	$scope.showHome = function(){
+		activeHome = "active";
+		activeTata = "";
+		activeFamily = "";
+		activeTataPoint = "";
+	};
+
+	$scope.showTata = function(){
+		activeHome = "";
+		activeTata = "active";
+		activeFamily = "";
+		activeTataPoint = "";
+	};
+	
+	$scope.showFamily = function(){
+		activeHome = "";
+		activeTata = "";
+		activeFamily = "active";
+		activeTataPoint = "";
+	};
+	
+	$scope.showTataPoint = function(){
+		activeHome = "";
+		activeTata = "";
+		activeFamily = "";
+		activeTataPoint = "active";
+	};
+	
 }]);
