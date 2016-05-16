61b11c8788bc452fb40b499270f0022dedd9c667
mirkoperillo <mirko.perillo@gmail.com>
2016-05-16 11:00:14 +0200

    added untracked files

---
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/model/ServiceOffice.java
@@ -0,0 +1,17 @@
+package it.smartcommunitylab.tataapp.model;
+
+public class ServiceOffice {
+	private String name;
+
+	public ServiceOffice(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
