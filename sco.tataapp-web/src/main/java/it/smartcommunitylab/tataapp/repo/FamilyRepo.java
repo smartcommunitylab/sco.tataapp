1df742ed121090e14f3f1a57707c1cdda90ea3ff
mirkoperillo <mirko.perillo@gmail.com>
2016-05-17 17:34:57 +0200

    clean code

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/repo/FamilyRepo.java
+++
@@ -1,9 +0,0 @@
-package it.smartcommunitylab.tataapp.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.smartcommunitylab.tataapp.model.Family;
-
-public interface FamilyRepo extends CrudRepository<Family, String> {
-
-}
