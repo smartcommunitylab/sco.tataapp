cdbb2c37363599de7dfc0e43987e3851cee3041d
mirkoperillo <mirko.perillo@gmail.com>
2016-05-17 15:47:07 +0200

    end matching algorithm

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/model/TimeSlot.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/model/TimeSlot.java
@@ -1,21 +1,27 @@
 package it.smartcommunitylab.tataapp.model;
 
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
 public enum TimeSlot {
 	MORNING(8, 12), AFTERNOON(12, 18), EVENING(18, 22), NIGHT(22, 8);
 
-	private final int fromHour;
-	private final int toHour;
+	private final long fromHour;
+	private final long toHour;
 
 	TimeSlot(int fromHour, int toHour) {
-		this.fromHour = fromHour;
-		this.toHour = toHour;
+		LocalDate epoch = new LocalDate(1970, 1, 1);
+		LocalTime fromTime = new LocalTime(fromHour, 0);
+		LocalTime toTime = new LocalTime(toHour, 0);
+		this.fromHour = epoch.toDateTime(fromTime).getMillis();
+		this.toHour = epoch.toDateTime(toTime).getMillis();
 	}
 
-	public int getFromHour() {
+	public long getFromHour() {
 		return fromHour;
 	}
 
-	public int getToHour() {
+	public long getToHour() {
 		return toHour;
 	}
 }
