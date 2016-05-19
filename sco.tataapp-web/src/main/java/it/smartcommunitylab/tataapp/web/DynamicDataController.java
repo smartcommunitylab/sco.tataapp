9daa3f73beefeb063668abf7cb45fcc4aed67d9e
mirkoperillo <mirko.perillo@gmail.com>
2016-05-19 12:06:21 +0200

    rest API for pricelist configuration

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/DynamicDataController.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/DynamicDataController.java
@@ -2,15 +2,17 @@ package it.smartcommunitylab.tataapp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.smartcommunitylab.tataapp.model.PriceList;
 import it.smartcommunitylab.tataapp.sec.IdentityLookupService;
 import it.smartcommunitylab.tataapp.service.DynamicDataService;
 
-//@RestController
-//@CrossOrigin
+@RestController
+// @CrossOrigin
 public class DynamicDataController {
 
 	@Autowired
@@ -19,9 +21,16 @@ public class DynamicDataController {
 	@Autowired
 	private IdentityLookupService identityLookup;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/public/api/agency/{agencyId}/pricelist")
+	@RequestMapping(method = RequestMethod.GET, value = "/console/api/agency/{agencyId}/pricelist")
 	public PriceList getPriceList(@PathVariable String agencyId) {
 		agencyId = identityLookup.getName();
 		return service.getPriceList(agencyId);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/console/api/agency/{agencyId}/pricelist")
+	public PriceList savePriceList(@RequestBody PriceList priceList, @PathVariable String agencyId) {
+		agencyId = identityLookup.getName();
+		priceList.setAgencyId(agencyId);
+		return service.savePriceList(agencyId, priceList);
 	}
 }
