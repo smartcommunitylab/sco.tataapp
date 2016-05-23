82b19dd11b4dff6c0e85eeaec6165ebcc6f36040
mirkoperillo <mirko.perillo@gmail.com>
2016-05-23 11:37:33 +0200

    added rest API to upload/download tata profile image

--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/BabysitterController.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/BabysitterController.java
@@ -1,9 +1,17 @@
 package it.smartcommunitylab.tataapp.web;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 import it.smartcommunitylab.tataapp.model.Babysitter;
 import it.smartcommunitylab.tataapp.sec.IdentityLookupService;
 import it.smartcommunitylab.tataapp.service.BabysitterService;
+import it.smartcommunitylab.tataapp.service.ImageService;
 
 @RestController
-@CrossOrigin
 public class BabysitterController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BabysitterController.class);
 
 	@Autowired
 	private BabysitterService service;
+
+	@Autowired
+	private ImageService imageSrv;
 
 	@Autowired
 	private IdentityLookupService identityLookup;
@@ -49,37 +62,43 @@ public class BabysitterController {
 		service.delete(agencyId, babysitterId);
 	}
 
-	// @RequestMapping(method = RequestMethod.POST, value =
-	// "/public/api/agency/{agencyId}/tata/search")
-	// public Page<Babysitter> search(@RequestBody SearchCriteria criteria,
-	// @PathVariable String agencyId,
-	// Pageable pageable) {
-	// agencyId = identityLookup.getName();
-	// return service.loadAll(agencyId, pageable);
-	// }
+	@RequestMapping(method = RequestMethod.POST, value = "/console/api/agency/{agencyId}/tata/{babysitterId}/avatar")
+	public void updloadProfilePicture(@PathVariable String agencyId, @PathVariable String babysitterId,
+			HttpServletRequest request, HttpServletResponse response) {
+		agencyId = identityLookup.getName();
+		try {
+			imageSrv.store(request.getInputStream(), babysitterId);
+		} catch (IOException e) {
+			try {
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Problem storing avatar");
+			} catch (IOException e1) {
+				logger.error("IOException sending HTTP error uploading avatar");
+			}
+		}
+	}
 
-	// @RequestMapping(value = "/api/image/upload/png/{ownerId}/{objectId}",
-	// method = RequestMethod.POST)
-	// public @ResponseBody String uploadImage(@RequestParam("file")
-	// MultipartFile file,
-	// @PathVariable String ownerId, @PathVariable String objectId,
-	// HttpServletRequest request) throws Exception {
-	// if(!Utils.validateAPIRequest(request, dataSetSetup, storage)) {
-	// throw new UnauthorizedException("Unauthorized Exception: token not
-	// valid");
-	// }
-	// String name = objectId + ".png";
-	// if(logger.isInfoEnabled()) {
-	// logger.info("uploadImage:" + name);
-	// }
-	// if (!file.isEmpty()) {
-	// BufferedOutputStream stream = new BufferedOutputStream(new
-	// FileOutputStream(
-	// new File(imageUploadDir + "/" + name)));
-	// FileCopyUtils.copy(file.getInputStream(), stream);
-	// stream.close();
-	// }
-	// return "{\"status\":\"OK\"}";
-	// }
+	@RequestMapping(method = RequestMethod.GET, value = "/console/api/agency/{agencyId}/tata/{babysitterId}/avatar")
+	public void downloadProfilePicture(@PathVariable String agencyId, @PathVariable String babysitterId,
+			HttpServletResponse resp) {
+		agencyId = identityLookup.getName();
+		InputStream in;
+		try {
+			in = imageSrv.retrieveInputStream(babysitterId);
+			OutputStream o = resp.getOutputStream();
+			resp.setContentType("image/png");
+			byte[] buffer = new byte[1024];
+			int c = 0;
+			while ((c = in.read(buffer)) != -1) {
+				o.write(buffer, 0, c);
+			}
+		} catch (IOException e) {
+			try {
+				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Problem getting avatar for download");
+			} catch (IOException e1) {
+				logger.error("IOException sending HTTP error getting avatar for download");
+			}
+		}
+
+	}
 
 }
