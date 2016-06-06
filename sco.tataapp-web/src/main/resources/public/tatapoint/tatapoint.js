f6ecce4021a2b565aec7b30e37c693c68802c25f
mirkoperillo <mirko.perillo@gmail.com>
2016-06-06 10:33:05 +0200

    tatapoint calendar iframe

--- sco.tataapp-web/src/main/resources/public/tatapoint/tatapoint.js
+++ sco.tataapp-web/src/main/resources/public/tatapoint/tatapoint.js
@@ -11,25 +11,23 @@ angular.module('app.tatapoint', [ 'ngRoute', 'ngResource' ])
 
 .controller(
 		'TatapointCtrl',
-		[ '$rootScope', '$scope', '$sce',
-				function($rootScope, $scope, $sce) {
-					var tatapointCalId = '';
-					$scope.tatapointCalendarURL = $sce.trustAsResourceUrl("https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=it&amp;bgcolor=%23FFFFFF&src=" + tatapointCalId + "&amp;color=%230F4B38&amp;ctz=Europe%2FRome");
+		[ '$rootScope', '$scope', '$sce','Settings', 'SharedData',
+				function($rootScope, $scope, $sce, Settings, SharedData) {
+					var agencyId = SharedData.getAppId();
+					Settings.read({aid : agencyId},function(data) {
+						var tatapointCalId = data.tatapointCalId;
+						$scope.tatapointCalendarURL = $sce.trustAsResourceUrl("https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;hl=it&amp;bgcolor=%23FFFFFF&amp;src="+ tatapointCalId +"&amp;color=%232F6309&amp;ctz=Europe%2FRome");
+					});
 
 				} ])
 
-.factory('Tatapoint', [ '$resource', function($resource) {
-	return $resource('api/agency/:id/tatapoint/:tid', {
-		id : "tataApp"
+.factory('Settings', [ '$resource', function($resource) {
+	return $resource('console/api/agency/:aid/settings', {
+		aid : '@aid'
 	}, {
-		list : {
-			isArray : true,
-			method : 'get',
-			transformResponse : function(data, headers) {
-				return JSON.parse(data).content;
-			}
+		read: {
+			method : 'get'
 		}
 	}
-
 	);
 } ]);
