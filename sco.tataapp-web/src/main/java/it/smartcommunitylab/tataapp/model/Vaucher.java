575b4e3b5aba6c8b442a41f07ce38d92ad30c80c
mirkoperillo <mirko.perillo@gmail.com>
2016-06-17 10:51:05 +0200

    fix #25

---
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/model/Vaucher.java
@@ -0,0 +1,23 @@
+package it.smartcommunitylab.tataapp.model;
+
+public class Vaucher {
+	private String type;
+	private Double value;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
--- sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/PublicController.java
+++ sco.tataapp-web/src/main/java/it/smartcommunitylab/tataapp/web/PublicController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -29,6 +30,7 @@ import it.smartcommunitylab.tataapp.model.PriceList;
 import it.smartcommunitylab.tataapp.model.ServiceOffice;
 import it.smartcommunitylab.tataapp.model.Settings;
 import it.smartcommunitylab.tataapp.model.TataPoint;
+import it.smartcommunitylab.tataapp.model.Vaucher;
 import it.smartcommunitylab.tataapp.service.BabysitterService;
 import it.smartcommunitylab.tataapp.service.DynamicDataService;
 import it.smartcommunitylab.tataapp.service.EstimatationService;
@@ -159,6 +161,15 @@ public class PublicController {
 	@RequestMapping(method = RequestMethod.GET, value = "/api/agency/{agencyId}/pricelist")
 	public PriceList getPriceList(@PathVariable String agencyId) {
 		return dynamicSrv.getPriceList(agencyId);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/api/agency/{agencyId}/vauchers")
+	public List<Vaucher> getVaucherTable(@PathVariable String agencyId) {
+		Settings s = settingsSrv.loadSettings(agencyId);
+		if (s != null) {
+			return s.getVauchers();
+		}
+		return new ArrayList<>();
 	}
 
 }
