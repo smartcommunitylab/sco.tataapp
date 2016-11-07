c01917e973f1fc552677a1fa6d5a023a54628e93
oskarnrk <o.zambotti@fbk.eu>
2016-11-07 10:54:29 +0100

    Little fixes required

--- tataapp-mobile/www/js/controllers/FeeCtrl.js
+++ tataapp-mobile/www/js/controllers/FeeCtrl.js
@@ -1,156 +1,155 @@
 angular.module('tataapp.controllers.fee', [])
 
 .controller('FeeCtrl', function ($scope, BackendSrv) {
-    $scope.pricelist = {};
+  $scope.pricelist = {}
 
-    $scope.d2d = function (num) {
-        return parseFloat(Math.round(num * 100) / 100).toFixed(2);
-    };
+  $scope.d2d = function (num) {
+    return parseFloat(Math.round(num * 100) / 100).toFixed(2)
+  }
 
-    BackendSrv.getPricelist().then(
-        function (pricelist) {
-            $scope.pricelist = pricelist;
-        }
-    );
+  BackendSrv.getPricelist().then(
+    function (pricelist) {
+      $scope.pricelist = pricelist
+    }
+  )
 
-    BackendSrv.getVouchersList().then(
-        function (vouchersList) {
-            $scope.vouchersList = vouchersList;
-        }
-    );
+  BackendSrv.getVouchersList().then(
+    function (vouchersList) {
+      $scope.vouchersList = vouchersList
+    }
+  )
 })
 
 .controller('FeeEstimateCtrl', function ($scope, $state, $filter, $ionicPopup, $ionicScrollDelegate, ionicDatePicker, Config, Utils, BackendSrv) {
-    var minDurationMonths = 3;
-    $scope.dateFormat = Config.dateFormat;
-    var now = new Date();
-    var nowMinTo = angular.copy(now);
-    nowMinTo.setMonth(nowMinTo.getMonth() + minDurationMonths);
+  var minDurationMonths = 3
+  $scope.dateFormat = Config.dateFormat
+  var now = new Date()
+  var nowMinTo = angular.copy(now)
+  nowMinTo.setMonth(nowMinTo.getMonth() + minDurationMonths)
 
-    $scope.estimateform = {
-        dateFrom: now.getTime(),
-        dateTo: nowMinTo.getTime(),
-        disability: false,
-        bonusAssignee: false,
-        bonusType: 'type1',
-        weeklyHour: null,
-        types: {
-            'type1': $filter('translate')('type1'),
-            'type2': $filter('translate')('type2'),
-            'type3': $filter('translate')('type3')
+  $scope.estimateform = {
+    dateFrom: now.getTime(),
+    dateTo: nowMinTo.getTime(),
+    disability: false,
+    bonusAssignee: false,
+    bonusType: 'type1',
+    weeklyHour: null,
+    types: {
+      'type1': $filter('translate')('type1'),
+      'type2': $filter('translate')('type2'),
+      'type3': $filter('translate')('type3')
+    }
+  }
+    // 'type3': $filter('translate')('type3') + ' *'
+
+  var datePickerOptions = {
+    setLabel: $filter('translate')('set'),
+    todayLabel: $filter('translate')('today'),
+    closeLabel: $filter('translate')('close'),
+    mondayFirst: true,
+    weeksList: Utils.getWeeksList(),
+    monthsList: Utils.getMonthsList(),
+    templateType: 'popup', // or 'popup
+    from: new Date(),
+    showTodayButton: true,
+    dateFormat: Config.dateFormat,
+    closeOnSelect: false
+  }
+
+  $scope.pickDate = function (field) {
+    var dpo = angular.copy(datePickerOptions)
+
+    if (field === 'from') {
+      dpo.inputDate = new Date($scope.estimateform.dateFrom)
+      dpo.callback = function (val) {
+        $scope.estimateform.dateFrom = val
+        var minDateTo = new Date($scope.estimateform.dateFrom)
+        minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths)
+        if ($scope.estimateform.dateTo < minDateTo.getTime()) {
+          $scope.estimateform.dateTo = minDateTo.getTime()
         }
-    };
-    //'type3': $filter('translate')('type3') + ' *'
+      }
+    } else if (field === 'to') {
+      dpo.inputDate = new Date($scope.estimateform.dateTo)
+      dpo.callback = function (val) {
+        $scope.estimateform.dateTo = val
+        var minDateTo = new Date()
+        minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths)
+        if ($scope.estimateform.dateTo < minDateTo.getTime()) {
+          /*
+          $scope.estimateform.dateFrom = now;
+          $scope.estimateform.dateTo = minDateTo.getTime();
+          */
 
-    var datePickerOptions = {
-        setLabel: $filter('translate')('set'),
-        todayLabel: $filter('translate')('today'),
-        closeLabel: $filter('translate')('close'),
-        mondayFirst: true,
-        weeksList: Utils.getWeeksList(),
-        monthsList: Utils.getMonthsList(),
-        templateType: 'popup', // or 'popup
-        from: new Date(),
-        showTodayButton: true,
-        dateFormat: Config.dateFormat,
-        closeOnSelect: false
-    };
-
-    $scope.pickDate = function (field) {
-        var dpo = angular.copy(datePickerOptions);
-
-        if (field === 'from') {
-            dpo.inputDate = new Date($scope.estimateform.dateFrom);
-            dpo.callback = function (val) {
-                $scope.estimateform.dateFrom = val;
-                var minDateTo = new Date($scope.estimateform.dateFrom);
-                minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths);
-                if ($scope.estimateform.dateTo < minDateTo.getTime()) {
-                    $scope.estimateform.dateTo = minDateTo.getTime();
-                }
-            };
-        } else if (field === 'to') {
-            dpo.inputDate = new Date($scope.estimateform.dateTo);
-            dpo.callback = function (val) {
-                $scope.estimateform.dateTo = val;
-                var minDateTo = new Date();
-                minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths);
-                if ($scope.estimateform.dateTo < minDateTo.getTime()) {
-                    /*
-                    $scope.estimateform.dateFrom = now;
-                    $scope.estimateform.dateTo = minDateTo.getTime();
-                    */
-
-                    $ionicPopup.alert({
-                        title: $filter('translate')('popup_title_attention', 'uppercase'), // String. The title of the popup.
-                        template: $filter('translate')('popup_fee_minduration'),
-                        okText: $filter('translate')('ok'),
-                        okType: 'button-positive' // (default: 'button-positive')
-                    }).then(function () {
-                        var minDateTo = new Date($scope.estimateform.dateFrom);
-                        minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths);
-                        $scope.estimateform.dateTo = minDateTo.getTime();
-                    });
-                }
-            };
-        }
-
-        ionicDatePicker.openDatePicker(dpo);
-    };
-
-    var showSentPopup = function () {
-        return $ionicPopup.alert({
+          $ionicPopup.alert({
             title: $filter('translate')('popup_title_attention', 'uppercase'), // String. The title of the popup.
-            template: $filter('translate')('popup_fee_attention'),
-            okText: $filter('translate')('popup_button_understand'),
+            template: $filter('translate')('popup_fee_minduration'),
+            okText: $filter('translate')('ok'),
             okType: 'button-positive' // (default: 'button-positive')
-        });
-    };
+          }).then(function () {
+            var minDateTo = new Date($scope.estimateform.dateFrom)
+            minDateTo.setMonth(minDateTo.getMonth() + minDurationMonths)
+            $scope.estimateform.dateTo = minDateTo.getTime()
+          })
+        }
+      }
+    }
 
-    var form2request = function (form) {
-        // this is how to convert estimateform to search request
-        /*
-        long startDate;
-        long endDate;
-        boolean bonusAssignee;
-        String bonusType;
-        double weeklyHour;
-        */
-        var request = {};
+    ionicDatePicker.openDatePicker(dpo)
+  }
 
-        request.startDate = form.dateFrom;
-        request.endDate = form.dateTo;
-        request.disability = form.disability;
-        request.bonusAssignee = form.bonusAssignee;
-        request.bonusType = form.bonusType;
-        request.weeklyHour = form.weeklyHour;
+  var showSentPopup = function () {
+    return $ionicPopup.alert({
+      title: $filter('translate')('popup_title_attention', 'uppercase'), // String. The title of the popup.
+      template: $filter('translate')('popup_fee_attention'),
+      okText: $filter('translate')('popup_button_understand'),
+      okType: 'button-positive' // (default: 'button-positive')
+    })
+  }
 
-        return request;
-    };
+  var form2request = function (form) {
+    // this is how to convert estimateform to search request
+    /*
+    long startDate;
+    long endDate;
+    boolean bonusAssignee;
+    String bonusType;
+    double weeklyHour;
+    */
+    var request = {}
 
-    $scope.estimate = function () {
-        $scope.estimation = 0;
+    request.startDate = form.dateFrom
+    request.endDate = form.dateTo
+    request.disability = form.disability
+    request.bonusAssignee = form.bonusAssignee
+    request.bonusType = form.bonusType
+    request.weeklyHour = form.weeklyHour
 
-        showSentPopup().then(
-            function () {
-                var request = form2request($scope.estimateform);
+    return request
+  }
 
-                BackendSrv.getEstimation(request).then(
-                    function (results) {
-                        $scope.estimation = {
-                            bonusRate: Math.round(results.bonusRate * 100) / 100,
-                            estimation: Math.round(results.estimation * 100) / 100,
-                            totalRate: Math.round(results.totalRate * 100) / 100
-                        };
-                        $ionicScrollDelegate.resize();
-                        $ionicScrollDelegate.scrollBottom(true);
-                    },
-                    function (reason) {
-                        console.log(reason);
-                    }
-                );
+  $scope.estimate = function () {
+    $scope.estimation = 0
+
+    showSentPopup().then(
+      function () {
+        var request = form2request($scope.estimateform)
+
+        BackendSrv.getEstimation(request).then(
+          function (results) {
+            $scope.estimation = {
+              bonusRate: Math.round(results.bonusRate * 100) / 100,
+              estimation: Math.round(results.estimation * 100) / 100,
+              totalRate: Math.round(results.totalRate * 100) / 100
             }
-        );
-    };
-
-});
+            $ionicScrollDelegate.resize()
+            $ionicScrollDelegate.scrollBottom(true)
+          },
+          function (reason) {
+            console.log(reason)
+          }
+        )
+      }
+    )
+  }
+})
--- tataapp-mobile/www/js/controllers/HomeCtrl.js
+++ tataapp-mobile/www/js/controllers/HomeCtrl.js
@@ -1,73 +1,73 @@
 angular.module('tataapp.controllers.home', [])
 
 .controller('AppCtrl', function ($scope, $state, $ionicHistory) {
-    $scope.openLink = function (link) {
-        window.open(link, '_system');
-    };
+  $scope.openLink = function (link) {
+    window.open(link, '_system')
+  }
 
-    // alternative to <a href>
-    $scope.goTo = function (state, params, reload, root) {
-        if (!!root) {
-            $ionicHistory.nextViewOptions({
-                historyRoot: true
-            });
-        }
+  // alternative to <a href>
+  $scope.goTo = function (state, params, reload, root) {
+    if (root) {
+      $ionicHistory.nextViewOptions({
+        historyRoot: true
+      })
+    }
 
-        if (!!reload) {
-            $ionicHistory.clearCache().then(function () {
-                $state.go(state, params);
-            });
-        } else {
-            $state.go(state, params);
-        }
-    };
+    if (reload) {
+      $ionicHistory.clearCache().then(function () {
+        $state.go(state, params)
+      })
+    } else {
+      $state.go(state, params)
+    }
+  }
 })
 
 .controller('HomeCtrl', function ($scope, $state, $filter, Config, BackendSrv) {
-    $scope.swiperOptions = {
-        autoplay: 2000,
-        speed: 1000,
-        loop: true
-    };
+  $scope.swiperOptions = {
+    autoplay: 2000,
+    speed: 1000,
+    loop: true
+  }
 
-    $scope.homePages = [];
-    var homeCounter = 1;
-    while ($filter('translate')('home_' + homeCounter + '_t') != ('home_' + homeCounter + '_t')) {
-        var entry = {
-            title: 'home_' + homeCounter + '_t',
-            content: 'home_' + homeCounter + '_c',
-            state: $filter('translate')('home_' + homeCounter + '_p'),
-            image: 'img/homepage_0' + homeCounter + '.png'
-        };
-        $scope.homePages.push(entry);
-        homeCounter++;
+  $scope.homePages = []
+  var homeCounter = 1
+  while ($filter('translate')('home_' + homeCounter + '_t') !== ('home_' + homeCounter + '_t')) {
+    var entry = {
+      title: 'home_' + homeCounter + '_t',
+      content: 'home_' + homeCounter + '_c',
+      state: $filter('translate')('home_' + homeCounter + '_p'),
+      image: 'img/homepage_0' + homeCounter + '.png'
     }
+    $scope.homePages.push(entry)
+    homeCounter++
+  }
 })
 
 .controller('PointsCtrl', function ($scope) {
-    // TODO
+  // TODO
 })
 
 .controller('InfoCtrl', function ($scope, $stateParams, $filter, $ionicSlideBoxDelegate, BackendSrv) {
-    $scope.swiperOptions = {};
+  $scope.swiperOptions = {}
 
-    $scope.infoPages = [];
-    var infoCounter = 1;
-    while ($filter('translate')('info_' + infoCounter + '_t') != ('info_' + infoCounter + '_t')) {
-        var entry = {
-            title: 'info_' + infoCounter + '_t',
-            content: 'info_' + infoCounter + '_c'
-        };
-        $scope.infoPages.push(entry);
-        infoCounter++;
+  $scope.infoPages = []
+  var infoCounter = 1
+  while ($filter('translate')('info_' + infoCounter + '_t') !== ('info_' + infoCounter + '_t')) {
+    var entry = {
+      title: 'info_' + infoCounter + '_t',
+      content: 'info_' + infoCounter + '_c'
     }
+    $scope.infoPages.push(entry)
+    infoCounter++
+  }
 
-    $scope.zones = [];
-    BackendSrv.getZones().then(function (response) {
-        $scope.zones = response.content;
-    });
+  $scope.zones = []
+  BackendSrv.getZones().then(function (response) {
+    $scope.zones = response.content
+  })
 
-    if (!!$stateParams && !!$stateParams['page']) {
-        $ionicSlideBoxDelegate.slide($stateParams['page']);
-    }
-});
+  if (!!$stateParams && !!$stateParams['page']) {
+    $ionicSlideBoxDelegate.slide($stateParams['page'])
+  }
+})
